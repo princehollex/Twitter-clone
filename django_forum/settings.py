c5e6ed901f7cf575f7df5af661a92ab6ccc6ee31
princehollex <osobajnr@yahoo.com>
2022-11-07 15:49:26 -0500

    added middleware

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -47,9 +47,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
